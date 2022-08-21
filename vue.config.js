ffb9d25b47de5bd0c8ed30c7f39434b90d49a7f1
HolmesZ <holmeszhi@qq.com>
2022-08-21 15:08:47 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    host: 'localhost',
     port: 4399,
     open: true,
     proxy: {}
