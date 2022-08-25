e204757aac8daf4a7dc25fe0875f5ad4a889fb75
xuye <2578629807@qq.com>
2022-08-25 09:03:20 +0800

    文字样式

--- src/views/Main/Components/FreshPopular.vue
+++ src/views/Main/Components/FreshPopular.vue
@@ -7,7 +7,7 @@
       :class="[item.price ? 'cyan' : 'white']"
       :to="`/product/${item.id}`"
     >
-      <el-image :src="baseurl + item.mainImagePosition[0]" />
+      <el-image :src="baseurl + item.mainImagePosition[0]" style="width:306px;height:306px"/>
       <p class="name">{{ item.prodName }}</p>
       <p class="price">
         <slot name="price" :data="item.price"></slot>
@@ -17,18 +17,18 @@
 </template>
 
 <script>
-import { BaseUrl } from '@/api/util'
+import { BaseUrl } from "@/api/util";
 export default {
-  name: 'CommonPannel1',
+  name: "CommonPannel1",
   props: {
-    goodsData: Array
+    goodsData: Array,
   },
   data() {
     return {
-      baseurl: BaseUrl
-    }
-  }
-}
+      baseurl: BaseUrl,
+    };
+  },
+};
 </script>
 
 <style lang="less" scoped>
@@ -48,17 +48,20 @@ export default {
       box-shadow: 0 3px 8px rgba(0, 0, 0, 0.2);
       color: #c81623;
     }
-    img {
-      max-width: 100%;
-      max-height: 100%;
-      vertical-align: middle;
-    }
+    // img {
+    //   max-width: 100%;
+    //   max-height: 100%;
+    //   vertical-align: middle;
+    // }
     .name {
-      margin: 12px 0 10px;
+      width: 100%;
+      margin: 12px 20px 10px;
+      padding: 0px 20px;
       font-size: 22px;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-      overflow: hidden;
+      overflow: hidden; // 溢出部分隐藏
+      white-space: nowrap; // 文字不换行
+      text-overflow: ellipsis; // 显示省略号
+      text-align: center;
     }
     .price {
       font-size: 23px;
@@ -73,7 +76,7 @@ export default {
     }
   }
   .cyan {
-    background-color: #D5F5E3;
+    background-color: #d5f5e3;
   }
   .white {
     background-color: #fff;
